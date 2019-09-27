03c7c3543ae8ee24a2eb7a0180b7bb5b68ac0096
peterlpt <impeter1986@outlook.com>
2019-09-27 11:20:15 +0800

    根据walle命令增加支持直接配置渠道列表，增加ApkResigner.py支持携带渠道列表参数，以满足CI临时动态指定渠道列表打包的需求

--- config.py
+++ config.py
@@ -3,21 +3,23 @@
 
 #keystore信息
 #Windows 下路径分割线请注意使用\\转义
-keystorePath = "..path/your.keystore"
-keyAlias = "your keyAlias"
-keystorePassword = "your keystorePassword"
-keyPassword = "your keyPassword"
+keystorePath = "epost_keys.keystore"
+keyAlias = "ePost"
+keystorePassword = "cxyapp"
+keyPassword = "cnpost"
 
 #加固后的源文件名（未重签名）
-protectedSourceApkName = "your protected.apk"
+protectedSourceApkName = "epost_encrypted.apk"
 #加固后的源文件所在文件夹路径(...path),注意结尾不要带分隔符，默认在此文件夹根目录
-protectedSourceApkDirPath = ""
+protectedSourceApkDirPath = "app/build/outputs"
 #渠道包输出路径，默认在此文件夹Channels目录下
-channelsOutputFilePath = ""
+channelsOutputFilePath = "app/build/outputs/channels/"
+#指定的渠道列表配置，如有配置，则它的优先级高于channelFilePath，channelFilePath将会失效。渠道名间用英文逗号分隔。如"meituan,meituan2,meituan3"
+channelList = ""
 #渠道名配置文件路径，默认在此文件夹根目录
-channelFilePath = ""
-#额外信息配置文件（绝对路径或相对路径，例如/Users/mac/Desktop/walle360/config.json）
+channelFilePath = "app/channel/"
+#额外信息配置文件（绝对路径，例如/Users/mac/Desktop/walle360/config.json）
 #配置信息示例参看https://github.com/Meituan-Dianping/walle/blob/master/app/config.json
 extraChannelFilePath = ""
 #Android SDK buidtools path , please use above 25.0+
-sdkBuildToolPath = "/Users/mac/Library/Android/sdk/build-tools/26.0.2"
+sdkBuildToolPath = "/home/ciUser/apps/android_sdk/build-tools/28.0.3"
